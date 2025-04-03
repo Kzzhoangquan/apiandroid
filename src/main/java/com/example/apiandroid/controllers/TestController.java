0562f3113ea4b1da9a0ddb4c833d02e1b6a47019
Kzzhoangquan <kzzhoangquan@gmail.com>
2025-04-03 17:27:34 +0700

    add

--- src/main/java/com/example/apiandroid/controllers/TestController.java
+++ src/main/java/com/example/apiandroid/controllers/TestController.java
@@ -3,14 +3,13 @@ package com.example.apiandroid.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TestController {
-
-    @GetMapping("/test")
-    public String test(){
-        return "1";
-    }
-
-
+        @GetMapping("/test")
+        @ResponseBody
+        public String test() {
+            return "1";  // Trả về chuỗi thô mà không cần template
+        }
 }
